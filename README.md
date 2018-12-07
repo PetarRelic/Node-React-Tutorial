8f822e0436e1a374b97a9b046ece7ccf25904931
PRelic <petar.relic@eng.it>
2018-12-07 15:30:33 +0100

    First Day => Done first part of Google Auth, Nodemon installed

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # Node Tutorial
 
 Node.js advanced tutorial
+
+-Server side of Application
+-Installed nodemon (start the app by typing "npm run dev" command in terminal) 
--- index.js
+++ index.js
@@ -1,9 +1,29 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys');
+
 const app = express();
 
-app.get('/', (req,res) => {
-    res.send({bye: 'buddy'});
-});
+//authorization
+passport.use(new GoogleStrategy({
+    clientID: keys.googleClientID,
+    clientSecret: keys.googleClientSecret,
+    callbackURL: '/auth/google/callback'
+}, (accessToken, refreshToken, profile, done) => {
+    console.log('access Token', accessToken);
+    console.log('refresh token', refreshToken);
+    console.log('profile: ', profile);
+}));
+
+//route for auth
+app.get('/auth/google', passport.authenticate('google', {
+    scope: ['profile', 'email']
+}));
+
+//route for response of auth
+app.get('/auth/google/callback', passport.authenticate('google'));
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
