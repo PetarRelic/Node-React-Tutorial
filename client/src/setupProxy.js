00b8def744e897fe67dd4a5eb46fedb88a8da4a7
PRelic <petar.relic@eng.it>
2018-12-11 15:54:50 +0100

    Third Day - More Code Formatting, Production Environment Made, React added

---
+++ client/src/setupProxy.js
@@ -0,0 +1,6 @@
+//this is newer version of proxying , instead of older version of proxying in package.json as a object
+const proxy = require('http-proxy-middleware');
+
+module.exports = function(app) {
+    app.use(proxy('/auth/google', { target: 'http://localhost:5000' }));
+};
